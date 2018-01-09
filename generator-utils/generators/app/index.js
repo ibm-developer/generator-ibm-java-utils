8e9b740303269ed4124ff13eeceb6fe8e5613b25
Katherine Stanley <katheris@uk.ibm.com>
2018-01-09 17:48:47 +0000

    Use java generator prompts to build questions
    Signed-off-by: Katherine Stanley <katheris@uk.ibm.com>

--- generator-utils/generators/app/index.js
+++ generator-utils/generators/app/index.js
@@ -17,18 +17,41 @@
 'use strict'
 
 const Generator = require('yeoman-generator')
+const promptmanager = require('../lib/promptmgr')
+const configmodule = require('ibm-java-codegen-common').config
+const javagen = require('generator-ibm-java')
+const javadefaults = javagen.defaults
+const handlebars = require('handlebars')
 
 module.exports = class extends Generator {
 
+  initializing() {
+    let defaults = new javadefaults()
+    defaults.setOptions(this)
+    this.data = new configmodule(defaults)
+    this.promptmgr = new promptmanager(this.data)
+    this.promptmgr.add('testprompt')
+    javagen.prompts.forEach(prompt => {
+      this.promptmgr.add(prompt);
+    })
+  }
+
   prompting() {
-    return this.prompt([{
-        type    : 'input',
-        name    : 'name',
-        message : 'Type a test input',
-        default : 'test'
-      }
-    ]).then((answers) => {
-        this.log('input', answers.name);
-    });
+    let qns = this.promptmgr.getQuestions()
+    return this.prompt(this.promptmgr.getQuestions()).then((answers) => {
+        this.promptmgr.afterPrompt(answers, this.data)
+    })
+  }
+
+  configuring() {
+    this.data.bluemix = JSON.stringify(this.data.bluemix)
+  }
+
+  writing() {
+    let template = this.fs.read(this.templatePath('generationscript.txt'));
+    let outfile = this.destinationPath('generated/generationscript.txt');
+    let compiledTemplate = handlebars.compile(template);
+    let output = compiledTemplate(this.data);
+    this.fs.write(outfile, output);
   }
 }
